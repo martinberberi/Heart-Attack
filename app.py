62c517ea4cf96c54f18380796390c319151fdcc3
Martin Berberi <74025390+martinberberi@users.noreply.github.com>
2021-04-13 23:02:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import pickle
+import os
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -101,8 +102,9 @@ def predict():
     else:
         return render_template('home.html')
     
-if __name__ == '__main__':
-    app.run(debug =True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
             
             
             
